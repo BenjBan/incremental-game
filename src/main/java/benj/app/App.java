bbf100acc2f6ab6d38156e14c0120687688a70af
Benj Baniqued <BaniquedBenj@gmail.com>
2025-10-24 13:05:36 +1300

    feat: add a blank window with the game's title on the screen
    closes #1

--- src/main/java/benj/app/App.java
+++ src/main/java/benj/app/App.java
@@ -1,5 +1,17 @@
 package main.java.benj.app;
 
-public class App {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class App extends JFrame {
+    public App() {
+        JLabel label = new JLabel("Welcome to the Incremental Game!");
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        setTitle("Incremental Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        add(label);
+        setVisible(true);
+    }
 }
