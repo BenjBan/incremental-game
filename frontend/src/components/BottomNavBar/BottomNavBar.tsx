471341dd25e42c8eb92be488e8f8c4611d6364ee
Benj Baniqued <BaniquedBenj@gmail.com>
2025-11-29 14:44:45 +1300

    feat: add bottom-nav component. Create layout for world1.

---
+++ frontend/src/components/BottomNavBar/BottomNavBar.tsx
@@ -0,0 +1,13 @@
+import './BottomNavBar.scss'
+
+export default function BottomNavBar() {
+    return (
+        <nav className="bottom-nav-bar">
+            <div className="nav-item">Upgrade 1</div>
+            <div className="nav-item">Upgrade 2</div>
+            <div className="nav-item">Upgrade 3</div>
+            <div className="nav-item">Upgrade 4</div>
+            <div className="nav-item">Upgrade 5</div>
+        </nav>
+    );
+}
--- frontend/src/pages/World1/World1.tsx
+++ frontend/src/pages/World1/World1.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import TopNavBar from '../../components/TopNavBar/TopNavBar'
+import BottomNavBar from '../../components/BottomNavBar/BottomNavBar'
 import './World1.scss'
 
 export default function World1() {
@@ -11,10 +12,20 @@ export default function World1() {
 
     return (
         <>
-            <TopNavBar />
-            <div style={{ padding: '20px', textAlign: 'center' }}>
-                <button onClick={back} className="play-button">Return</button>
+            <header>
+                <TopNavBar />
+            </header>
+            <div className="stats-display">
             </div>
+            <div className="content">
+                {/* temporary button for testing */}
+                <div style={{ padding: '20px', textAlign: 'center' }}>
+                    <button onClick={back} className="play-button">Return</button>
+                </div>
+            </div>
+            <footer>
+                <BottomNavBar />
+            </footer>
         </>
     )
 }
